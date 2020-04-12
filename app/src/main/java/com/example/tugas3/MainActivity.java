eedccd01042f250281941cbb3b57e1ab1a98071e
dayudianmitha <dayudianparamitha17@gmail.com>
2020-04-12 22:22:29 +0800

    Tugas RecyclerView

---
+++ app/src/main/java/com/example/tugas3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tugas3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.adidas,R.drawable.chanel,R.drawable.dior,R.drawable.gucci,R.drawable.nike,R.drawable.uniqlo,R.drawable.zara};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.name_brand);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
